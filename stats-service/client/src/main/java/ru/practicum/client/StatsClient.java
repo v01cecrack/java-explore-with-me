030e71a9f890279e17fbd77c136c9b7d65cdc597
Kostya <kostya2283221337@yandex.ru>
2023-09-11 03:54:24 +0300

    fixed

--- stats-service/client/src/main/java/ru/practicum/client/StatsClient.java
+++ stats-service/client/src/main/java/ru/practicum/client/StatsClient.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
-import ru.practicum.service.dto.HitDto;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
