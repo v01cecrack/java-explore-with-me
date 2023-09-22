6423b459f5cc5814027e1ac4898fedca8188b183
Kostya <kostya2283221337@yandex.ru>
2023-09-22 20:14:51 +0300

    final3

--- main-service/src/main/java/ru/practicum/mainservice/exception/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/mainservice/exception/ErrorHandler.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ValidationException;
+
 @RestControllerAdvice
 public class ErrorHandler {
     @ExceptionHandler
