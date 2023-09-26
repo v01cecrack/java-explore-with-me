53b10c310013c79e2fa785bab1bce23e1705fc59
Kostya <kostya2283221337@yandex.ru>
2023-09-26 18:16:33 +0300

    checkstyle fixed

--- main-service/src/main/java/ru/practicum/mainservice/event/dto/EventRequestStatusUpdateResult.java
+++ main-service/src/main/java/ru/practicum/mainservice/event/dto/EventRequestStatusUpdateResult.java
@@ -2,7 +2,6 @@ package ru.practicum.mainservice.event.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Value;
 import ru.practicum.mainservice.request.dto.ParticipationRequestDto;
 
